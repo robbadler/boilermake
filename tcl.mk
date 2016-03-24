4e0fc4f737ef59cb1f33e45a651dd7239cf041e7
Sarge <sarge_dodge@mentor.com>
2016-03-24 14:55:23 -0700

    Fix path to Tcl imports.

--- tcl.mk
+++ tcl.mk
@@ -8,9 +8,9 @@ TCL_LIBDIR   := -L$(TCL_HOME)/lib
 TCL_LIB      := -ltcl
 else
 # Tcl 8.6 export dir
-TCL_HOME := /net/orw-cicd57-vm/scratch1/continuous_trunk/dev/pxopen_superproj/$(VCO)/Mgc_home
-TCL_INCDIRS  := $(TCL_HOME)/shared/include/tcl8.6
+TCL_HOME := /project/dsm/cicd/icf_wg_server/ic/tcl86/exports.v8-6_0-0-1_engr-$(VCO)/mgc_home
+TCL_INCDIRS  := $(TCL_HOME)/shared/pkgs/tcl86_inhouse.$(VCO)/include/tcl8.6
 TCL_INCLUDES := -isystem$(TCL_INCDIRS)
-TCL_LIBDIR   := -L$(TCL_HOME)/pkgs/tcl86/lib
+TCL_LIBDIR   := -L$(TCL_HOME)/pkgs/tcl86.$(VCO)/lib
 TCL_LIB      := -ltcl
 endif
