338d2ad40b22498639d16fe6ac6815d499430163
Sarge <sarge_dodge@mentor.com>
2016-03-28 12:10:38 -0700

    Add prototypical support for Qt 5.

--- qt.mk
+++ qt.mk
@@ -1,8 +1,30 @@
+QT_VERSION ?= 4
+ifneq ($(QT_VERSION), 5)
 # Qt install dir
 QT_HOME := /wv/ic_wg_server/ic/qt/exports.v0-0_3-10-2015_engr-$(VCO)/mgc_home
 
 # Qt binaries
 QT_BIN := $(QT_HOME)/shared/pkgs/icv_qt_comp_inhouse.$(VCO)/bin
+
+# Compile
+QT_INCDIRS := $(QT_HOME)/shared/pkgs/icv_qt_comp_inhouse.$(VCO)/include
+
+# Link
+QT_LIBDIR   := -L$(QT_HOME)/pkgs/icv_lib.$(VCO)/lib64
+else
+# Qt install dir
+QT_HOME := /wv/calibre_3rdparty/QT/qt-5.6.0/$(VCO)
+
+# Qt binaries
+QT_BIN := $(QT_HOME)/bin
+
+# Compile
+QT_INCDIRS := $(QT_HOME)/include
+
+# Link
+QT_LIBDIR   := -L$(QT_HOME)/lib
+endif
+
 # Meta-Object Compiler
 MOC = $(QT_BIN)/moc
 # Resource compiler
@@ -11,11 +33,9 @@ RCC = $(QT_BIN)/rcc
 UIC = $(QT_BIN)/uic
 
 # Compile
-QT_INCDIRS := $(QT_HOME)/shared/pkgs/icv_qt_comp_inhouse.$(VCO)/include
 QT_INCLUDES := -isystem$(QT_INCDIRS)
 
 # Link
-QT_LIBDIR   := -L$(QT_HOME)/pkgs/icv_lib.$(VCO)/lib64
 QT_CORE_LIB := -lQtCore -lQtXml
 QT_GUI_LIB  := -lQtGui $(QT_CORE_LIB)
 QT_NETWORK_LIB := -lQtNetwork $(QT_GUI_LIB)
