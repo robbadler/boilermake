1e7525877393fe379063f3ce11344b8dfc56ed56
Sarge <sarge_dodge@mentor.com>
2016-04-04 14:13:03 -0700

    Switch to imported core geom.

--- coregeom.mk
+++ coregeom.mk
@@ -1,4 +1,4 @@
 COREGEOM_HOME ?= /wv/icfbld/builds/TOT/Rook/CoreGeom/dev/cicd/cgeom/$(VCO)/exports/mgc_home
-COREGEOM_INCDIRS = $(COREGEOM_HOME)/shared/pkgs/cgeom_inhouse.$(VCO)/include/CgeomAPI
+COREGEOM_INCDIRS = $(COREGEOM_HOME)/shared/pkgs/cgeom_inhouse.$(VCO)/include
 COREGEOM_LIBDIR =  -L$(COREGEOM_HOME)/pkgs/cgeom.$(VCO)/lib/cgeom
 COREGEOM_LIB = -loap_coregeom.so
