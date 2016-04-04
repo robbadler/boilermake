47e4f6866bf08b39a5a29c283278c7f90f924f9f
Sarge <sarge_dodge@mentor.com>
2016-04-04 14:45:20 -0700

    Switch to imported core geom.

--- boost.mk
+++ boost.mk
@@ -1,4 +1,4 @@
 BOOST_HOME=/project/dsm/cicd/icf_wg_server/fw_ic/boost/exports.v1-43_0-0-1_engr-$(VCO)/mgc_home
 BOOST_INCDIRS := $(BOOST_HOME)/shared/pkgs/boost_inhouse.$(VCO)/boost_ext/include/boost
 BOOST_INCLUDES := -isystem$(BOOST_INCDIRS)
-BOOST_LIBDIR =  -L$(BOOST_HOME)/pkgs/boost.$(VCO)
+BOOST_LIBDIR =  -L$(BOOST_HOME)/pkgs/boost.$(VCO)/lib
