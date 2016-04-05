56290e3666c26c883a46ba5479be11f08eeacd34
Robert Houghton <rhoughto@mentor.com>
2016-04-05 11:21:55 -0700

    liboap_coregeom.so is in its own project. Get it from MGC_HOME for now, from the WG server later

--- coreGeom.mk
+++ coreGeom.mk
@@ -1,6 +1,6 @@
 
-CORE_GEOM_INCDIRS  := $(MGC_HOME)/shared/include/InfrastructureAPI/
+CORE_GEOM_INCDIRS  := $(MGC_HOME)/shared/pkgs/cgeom_inhouse.$(VCO)/include
 CORE_GEOM_INCLUDES := -isystem$(CORE_GEOM_INCDIRS)
-CORE_GEOM_LIBDIR   := -L$(MGC_HOME)/pkgs/infrastructure/lib/Infrastructure
+CORE_GEOM_LIBDIR   := -L$(MGC_HOME)/pkgs/cgeom.$(VCO)/lib/cgeom
 CORE_GEOM_LIB := -loap_coregeom
 #-loap_oa_utils -loap_instancestack -loap_executor
