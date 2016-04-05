7900c8ca86353f4e4fde09b95a0d623cbf0e2b18
icfbuild <icfbuild@mentorg.com>
2016-04-05 14:32:45 -0700

    adding VCO support to Tcl import file

--- tcl.mk
+++ tcl.mk
@@ -1,4 +1,4 @@
-TCL_INCDIRS  := $(MGC_HOME)/shared/pkgs/tcl86_inhouse/include/tcl8.6
+TCL_INCDIRS  := $(MGC_HOME)/shared/pkgs/tcl86_inhouse.$(VCO)/include/tcl8.6
 TCL_INCLUDES := -isystem$(TCL_INCDIRS)
-TCL_LIBDIR   := -L$(MGC_HOME)/pkgs/tcl86/lib
+TCL_LIBDIR   := -L$(MGC_HOME)/pkgs/tcl86.$(VCO)/lib
 TCL_LIB      := -ltcl8.6
