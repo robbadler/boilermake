84c34494338a185aecc97f6905005b6c91ff1b0d
Sarge <sarge_dodge@mentor.com>
2016-04-06 14:55:39 -0700

    Switch to the exported version of core geometry.

--- coregeom.mk
+++ coregeom.mk
@@ -1,4 +1,4 @@
-COREGEOM_HOME ?= /wv/icfbld/builds/TOT/Rook/CoreGeom/dev/cicd/cgeom/$(VCO)/exports/mgc_home
+COREGEOM_HOME ?= /wv/icbld8/icfw_wg_server/cicd/cgeom/exports.v1-0_0-0-1_engr-$(VCO)/mgc_home
 COREGEOM_INCDIRS = $(COREGEOM_HOME)/shared/pkgs/cgeom_inhouse.$(VCO)/include
 COREGEOM_LIBDIR =  -L$(COREGEOM_HOME)/pkgs/cgeom.$(VCO)/lib/cgeom
 COREGEOM_LIB = -loap_coregeom
