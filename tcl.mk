f9fdc2bc58fc043d324688ec026ac016d41adb42
Robert Houghton <rhoughto@mentor.com>
2016-04-11 08:31:11 -0700

    there is no Tcl8.6xxxx0.2 for aoh, only aoi. So use the xxx.0.1 version

--- tcl.mk
+++ tcl.mk
@@ -1,4 +1,4 @@
-TCL_HOME     := /project/dsm/cicd/icf_wg_server/ic/tcl86/exports.v8-6_0-0-2_engr-$(VCO)/mgc_home
+TCL_HOME     := /project/dsm/cicd/icf_wg_server/ic/tcl86/exports.v8-6_0-0-1_engr-$(VCO)/mgc_home
 TCL_INCDIRS  := $(TCL_HOME)/shared/pkgs/tcl86_inhouse.$(VCO)/include/tcl8.6
 TCL_INCLUDES := -isystem$(TCL_INCDIRS)
 TCL_LIBDIR   := -Wl,-rpath-link=$(TCL_HOME)/pkgs/tcl86.$(VCO)/lib \
