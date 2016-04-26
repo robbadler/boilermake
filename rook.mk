c677376d8bc33fcc75dabc77b82d058fac856bd8
Sarge <sarge_dodge@mentor.com>
2016-04-26 12:40:15 -0700

    Roll version to v1.5.2.

--- rook.mk
+++ rook.mk
@@ -8,7 +8,7 @@
 #   ROOK_HOME=/net/bokeh/scratch1/rei/rook
 #   export ROOK_HOME
 #
-ROOK_HOME ?= /wv/sarge/exports/rook_1_4_1.$(VCO)
+ROOK_HOME ?= /wv/sarge/exports/rook_1_5_1.$(VCO)
 ROOK_INCDIRS = $(ROOK_HOME)/mgc_home/shared/pkgs/rook_inhouse.$(VCO)/include
 ROOK_LIBDIR =  -L$(ROOK_HOME)/mgc_home/pkgs/rook.$(VCO)/lib
 ROOK_LIB = -lrook
