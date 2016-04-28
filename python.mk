7ab23046ce0836e4907755fc7ce2ce196ff22d14
Sarge <sarge_dodge@mentor.com>
2016-04-28 15:26:54 -0700

    Add support for different versions of Python.

--- python.mk
+++ python.mk
@@ -1,6 +1,15 @@
-# Python export dir
+ifeq ($(PYTHON_VERSION), 2.6)
+# Python 2.6 export dir
 PYTHON_HOME := /project/dsm/cicd/icf_wg_server/ic/python/exports.v1-0_0-0-5_engr-$(VCO)/mgc_home
 PYTHON_INCDIRS  := $(PYTHON_HOME)/pkgs/python.$(VCO)/include/python2.6
 PYTHON_INCLUDES := -isystem$(PYTHON_INCDIRS)
 PYTHON_LIBDIR   := -L$(PYTHON_HOME)/pkgs/python.$(VCO)/lib
 PYTHON_LIB      := -lpython2.6
+else
+# Python 3 export dir
+PYTHON_HOME := /project/dsm/cicd/icf_wg_server/ic/python/exports.v1-0_0-0-5_engr-$(VCO)/mgc_home
+PYTHON_INCDIRS  := $(PYTHON_HOME)/pkgs/python.$(VCO)/include/python2.6
+PYTHON_INCLUDES := -isystem$(PYTHON_INCDIRS)
+PYTHON_LIBDIR   := -L$(PYTHON_HOME)/pkgs/python.$(VCO)/lib
+PYTHON_LIB      := -lpython2.6
+endif
