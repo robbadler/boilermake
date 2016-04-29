6f2cad69dc395a58f574c9badb2c22ba504b78cf
Sarge <sarge_dodge@mentor.com>
2016-04-29 13:43:14 -0700

    Add Python 3 compilation support.

--- python.mk
+++ python.mk
@@ -7,9 +7,9 @@ PYTHON_LIBDIR   := -L$(PYTHON_HOME)/pkgs/python.$(VCO)/lib
 PYTHON_LIB      := -lpython2.6
 else
 # Python 3 export dir
-PYTHON_HOME := /project/dsm/cicd/icf_wg_server/ic/python/exports.v1-0_0-0-5_engr-$(VCO)/mgc_home
-PYTHON_INCDIRS  := $(PYTHON_HOME)/pkgs/python.$(VCO)/include/python2.6
+PYTHON_HOME := /wv/sarge/Python-3.5.1
+PYTHON_INCDIRS  := $(PYTHON_HOME)/include
 PYTHON_INCLUDES := -isystem$(PYTHON_INCDIRS)
-PYTHON_LIBDIR   := -L$(PYTHON_HOME)/pkgs/python.$(VCO)/lib
-PYTHON_LIB      := -lpython2.6
+PYTHON_LIBDIR   := -L$(PYTHON_HOME)/lib
+PYTHON_LIB      := -lpython3.5m
 endif
