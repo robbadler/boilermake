2e735d61d5a86596a4934d6ba1ff4281aa96b0ce
Robert Houghton <rhoughto@mentor.com>
2016-05-12 10:18:56 -0700

    use more recent tcl for AOI than AOH

--- tcl.mk
+++ tcl.mk
@@ -1,4 +1,9 @@
+ifeq ($(VCO),aoi)
+TCL_HOME     := /project/dsm/cicd/icf_wg_server/ic/tcl86/exports.v8-6_0-0-2_engr-$(VCO)/mgc_home
+else
 TCL_HOME     := /project/dsm/cicd/icf_wg_server/ic/tcl86/exports.v8-6_0-0-1_engr-$(VCO)/mgc_home
+endif
+
 TCL_INCDIRS  := $(TCL_HOME)/shared/pkgs/tcl86_inhouse.$(VCO)/include/tcl8.6
 TCL_INCLUDES := -isystem$(TCL_INCDIRS)
 TCL_LIBDIR   := -Wl,-rpath-link=$(TCL_HOME)/pkgs/tcl86.$(VCO)/lib \
